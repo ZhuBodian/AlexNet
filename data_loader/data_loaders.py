156fcbe034a80bce11aeb9014accc71765d1d2b5
zhuBoDian <379837872@qq.com>
2022-05-07 01:39:27 +0800

    部分完成train，val的不同transform的功能

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,5 +1,7 @@
 import torch
 from torchvision import datasets, transforms
+from torch.utils.data.sampler import SubsetRandomSampler
+import utils
 from base import BaseDataLoader
 import os, json
 import pandas as pd
@@ -7,6 +9,7 @@ from torch.utils.data.dataset import Dataset
 from PIL import Image
 import numpy as np
 from utils import util
+from sklearn.model_selection import StratifiedShuffleSplit
 
 
 class MnistDataLoader(BaseDataLoader):
@@ -22,6 +25,7 @@ class MnistDataLoader(BaseDataLoader):
         self.data_dir = data_dir
         self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+        a = 1
 
 
 class TinyImageNetDataloader(BaseDataLoader):
@@ -29,36 +33,83 @@ class TinyImageNetDataloader(BaseDataLoader):
     MiniImageNet data loading demo using BaseDataLoader
     """
 
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True,
+                 assign_val_sample=False):
         # 这里首先令训练集与验证集具有相同的trsfm
-        trsfm = transforms.Compose([transforms.RandomResizedCrop(224),
-                                    transforms.ToTensor(),
-                                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+        trsfm = {
+            "train": transforms.Compose([transforms.Resize([224, 224]),
+                                         transforms.RandomHorizontalFlip(),
+                                         transforms.ToTensor(),
+                                         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
+            "val": transforms.Compose([transforms.Resize([224,224]),
+                                       transforms.ToTensor(),
+                                       transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])}
         self.data_dir = data_dir
-        self.dataset = TinyImageNetDatasets(data_dir=self.data_dir, train=training, transform=trsfm)
-        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+        self.dataset = TinyImageNetDatasets(path=self.data_dir, train=training, transform=trsfm, split=validation_split)
+        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers, assign_val_sample, self.samples)
 
 
 class TinyImageNetDatasets(Dataset):
-    def __init__(self, data_dir, train, transform, target_transfrom=None):
+    def __init__(self, path, train, transform, split):
         super().__init__()
 
-        image_dir = os.path.join(data_dir, "images")
+        image_dir = os.path.join(path, "images")
         assert os.path.exists(image_dir), "dir:'{}' not found.".format(image_dir)
 
-        self.target_transform = target_transfrom
+        self.target_transform = None
         self.image_folder = image_dir
         self.transform = transform
-        self.root = data_dir
+        self.root = path
 
-        mode = 'train' if train else 'test'
-        data_path = os.path.join(data_dir, mode + '_data.pickle')
-        target_path = os.path.join(data_dir, mode + '_target.pickle')
-        assert os.path.exists(data_path), "file:'{}' not found.".format(data_path)
-        assert os.path.exists(target_path), "file:'{}' not found.".format(target_path)
+        # 根据布尔值train，来确定是生成训练集数据（如果是训练集，那么肯定也要生成验证集），还是测试集数据
+        csv_mode = 'train' if train else 'test'
+        csv_name = csv_mode + '.csv'
+        csv_path = os.path.join(path, csv_name)
+        assert os.path.exists(csv_path), "file:'{}' not found. please run creat_tiny.py firstly".format(csv_path)
+        csv_data = pd.read_csv(csv_path)
 
-        self.data = util.load_from_pickle(data_path)
-        self.targets = util.load_from_pickle(target_path)
+        csv_image_name_list = list(csv_data['filename'])
+        csv_image_label_list = list(set(csv_data['label']))  # set具有唯一性，但是无序
+        csv_image_label_list.sort()  # 注意list的sort函数为就地sort
+
+        # 生成self.target
+        for id, image_label in enumerate(csv_image_label_list):
+            csv_data.replace(image_label, id, inplace=True)
+        self.targets = torch.from_numpy(np.array(csv_data['label'])).long()[:300]
+
+        self.samples = self.cal_samples(split, self.targets)
+
+        """之后的任务：探究不同数据增强对图片的影响，对训练结果的影响，并对pytorch-template进行修改，添加事先分隔号训练集、验证集的功能"""
+        data_list = []
+        for idx, image_name in enumerate(csv_image_name_list[:300]):
+            image_path = os.path.join(os.getcwd(), image_dir, image_name)
+            # 读出来的图像是RGBA四通道的，A通道为透明通道，该通道值对深度学习模型训练来说暂时用不到，因此使用convert(‘RGB’)进行通道转换
+            image = Image.open(image_path).convert('RGB')
+            image = transform['train'](image)  # .unsqueeze(0)增加维度（0表示，在第一个位置增加维度）
+
+            data_list.append(image)
+
+            ############################
+            # temp_trsfm = transforms.Compose([transforms.Resize(224)
+            #                                  transforms.RandomHorizontalFlip(),
+            #                                  transforms.ToTensor()])
+            # image_path = os.path.join(os.getcwd(), image_dir, image_name)
+            # image = Image.open(image_path).convert('RGB')
+            # image = temp_trsfm(image)
+            # transforms.ToPILImage()(image).show()
+
+        self.data = torch.stack(data_list)
+        del data_list
+
+        num_label2text_label = dict([(idx, text) for idx, text in enumerate(csv_image_label_list)])
+        # util.write_json(num_label2text_label, os.path.join(path, 'num_label2text_label.json'))
+
+    def cal_samples(self, rate, targets):
+        """事先分割好训练集与验证集，方便后续不同的transform"""
+        split = StratifiedShuffleSplit(n_splits=1, test_size=rate, random_state=42)
+        for train_index, valid_index in split.split(np.zeros(len(targets)), np.array(targets)):
+            train_sampler, valid_sampler = SubsetRandomSampler(train_index), SubsetRandomSampler(valid_index)
+        return [train_sampler, valid_sampler]
 
     def __len__(self):
         return len(self.targets)
